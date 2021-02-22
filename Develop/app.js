b277139ab9189ed2da1b23bf4873842454f5f191
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-02-22 11:36:05 -0600

    Email Return Added Throughout

--- Develop/app.js
+++ Develop/app.js
@@ -72,8 +72,7 @@ const engineerQs = [
             if(email.includes("@" && ".")) {
                 return true;
             }else {
-                console.log(" PLEASE TYPE IN A VALID E-MAIL ADDRESS.")
-                return false;
+                return "PLEASE TYPE IN A VALID E-MAIL ADDRESS."
             }
         }
     },
@@ -104,8 +103,7 @@ const internQs = [
             if(email.includes("@" && ".")) {
                 return true;
             }else {
-                console.log(" PLEASE TYPE IN A VALID E-MAIL ADDRESS.")
-                return false;
+                return "PLEASE TYPE IN A VALID E-MAIL ADDRESS."
             }
         }
     },
@@ -171,26 +169,3 @@ const init = (qM) => {
 
 init(managerQs);
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
